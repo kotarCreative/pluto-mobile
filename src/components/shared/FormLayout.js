d6ebfeeeab012ba48c5c31feef239ed886d0b75e
David Buss <kotarcreative@gmail.com>
2018-11-03 15:17:18 -0600

    Add dynamic form component.

---
+++ src/components/shared/FormLayout.js
@@ -0,0 +1,52 @@
+/**
+ * Standard button component
+ *
+ * @format
+ * @flow
+ */
+
+import React, { PureComponent } from 'react'
+import { StyleSheet, View } from 'react-native'
+import COLOR from '@/constants/colors'
+
+// styles
+const styles = StyleSheet.create({
+  form: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    width: 200
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderBottomColor: COLOR.WHITE,
+    padding: 10
+  },
+  footer: {
+    padding: 10
+  }
+})
+
+export class FormLayout extends PureComponent<Props, State> {
+  render() {
+    return (
+      <View style={styles.form}>
+        {this.renderChildren()}
+      </View>
+    )
+  }
+
+  renderChildren() {
+    const fields = this.props.children
+    let output = []
+    for (i = 0; i < fields.length - 1; i++) {
+      output.push(
+        <View style={styles.separator} key={'form_' + i}>
+          {fields[i]}
+        </View>
+      )
+    }
+    output.push(<View style={styles.footer}>{fields[i]}</View>)
+    return output
+  }
+}
